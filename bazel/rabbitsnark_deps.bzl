51287fcf38684ddaa2818a2dc294c5a51468c790
Ryan Kim <chokobole33@gmail.com>
2025-07-17 10:24:21 +0900

    feat(gnark): support proving key parsing
    Added support for parsing proving keys in gnark-compatible format.

--- bazel/rabbitsnark_deps.bzl
+++ bazel/rabbitsnark_deps.bzl
@@ -1,10 +1,10 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 def rabbitsnark_deps():
-    ZKX_COMMIT = "328f75d16d17c9e414322d4cfcfadf55852d44a5"
+    ZKX_COMMIT = "63c55b0cf18d0000d2c0b46c3d5dee3a5d480795"
     http_archive(
         name = "zkx",
-        sha256 = "c4ae29351b724190cbb12b4d51eaaa079d0d4c8b87449e063e12fcf74f4465bc",
+        sha256 = "815c528d493168cfd29493c9f109de12cba2e31a975f3cf5754f1c92ef69f110",
         strip_prefix = "zkx-{commit}".format(commit = ZKX_COMMIT),
         urls = ["https://github.com/zk-rabbit/zkx/archive/{commit}.tar.gz".format(commit = ZKX_COMMIT)],
     )
---
+++ gnark/reader_utils.cc
@@ -0,0 +1,27 @@
+#include "gnark/reader_utils.h"
+
+namespace zkx::gnark {
+
+// See
+// https://github.com/Consensys/gnark-crypto/blob/43897fd/utils/unsafe/dump_slice.go#L89-L102
+absl::Status ReadUnsafeMarker(const base::ReadOnlyBuffer& buffer) {
+  base::EndianAutoReset auto_reset(buffer, base::Endian::kNative);
+  uint64_t marker;
+  TF_RETURN_IF_ERROR(buffer.Read(&marker));
+  if (marker != 0xdeadbeef) {
+    return absl::NotFoundError("Invalid unsafe marker");
+  }
+  return absl::OkStatus();
+}
+
+absl::Status ReadElements(const base::ReadOnlyBuffer& buffer,
+                          std::vector<bool>* result) {
+  for (uint32_t i = 0; i < result->size(); ++i) {
+    bool value;
+    TF_RETURN_IF_ERROR(buffer.Read(&value));
+    (*result)[i] = value;
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace zkx::gnark
