27fadd802505213fef68d42f86f890bcc3a1c8d2
ashjeong <ashjeong@umich.edu>
2025-07-21 11:13:43 +0900

    feat(gnark): add bin proof/public writer

---
+++ gnark/bin/proof_writer.h
@@ -0,0 +1,50 @@
+#ifndef GNARK_BIN_PROOF_WRITER_H_
+#define GNARK_BIN_PROOF_WRITER_H_
+
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+
+#include "zkx/literal.h"
+
+namespace zkx::gnark {
+
+template <typename Curve>
+absl::Status WriteProofToBin(Literal& proof, const std::string& path) {
+  using G1AffinePoint = typename Curve::G1Curve::AffinePoint;
+  using G2AffinePoint = typename Curve::G2Curve::AffinePoint;
+  using Fq = typename G1AffinePoint::BaseField;
+  using F = typename G1AffinePoint::ScalarField;
+
+  std::vector<Literal> decomposed = proof.DecomposeTuple();
+  const G1AffinePoint& pi_a = decomposed[0].data<G1AffinePoint>()[0];
+  const G2AffinePoint& pi_b = decomposed[1].data<G2AffinePoint>()[0];
+  const G1AffinePoint& pi_c = decomposed[2].data<G1AffinePoint>()[0];
+
+  base::Uint8VectorBuffer write_buf;
+  TF_RETURN_IF_ERROR(write_buf.Grow(base::EstimateSize(
+      pi_a, pi_b, pi_c, uint32_t{0}, G1AffinePoint::Zero())));
+  write_buf.set_endian(base::Endian::kBig);
+  base::AutoReset<bool> reset_scalar_field_is_in_montgomery(
+      &base::Serde<F>::s_is_in_montgomery, false);
+  base::AutoReset<bool> reset_base_field_is_in_montgomery(
+      &base::Serde<Fq>::s_is_in_montgomery, false);
+  base::AutoReset<math::AffinePointSerdeMode> reset_g1_s_mode(
+      &base::Serde<G1AffinePoint>::s_mode,
+      math::AffinePointSerdeMode::kGnarkRaw);
+  base::AutoReset<math::AffinePointSerdeMode> reset_g2_s_mode(
+      &base::Serde<G2AffinePoint>::s_mode,
+      math::AffinePointSerdeMode::kGnarkRaw);
+
+  TF_RETURN_IF_ERROR(write_buf.WriteMany(pi_a, pi_b, pi_c, uint32_t{0},
+                                         G1AffinePoint::Zero()));
+
+  std::string proof_string(reinterpret_cast<const char*>(write_buf.buffer()),
+                           write_buf.buffer_offset());
+  return tsl::WriteStringToFile(tsl::Env::Default(), path, proof_string);
+}
+
+}  // namespace zkx::gnark
+
+#endif  // GNARK_BIN_PROOF_WRITER_H_
---
+++ gnark/bin/public_writer.h
@@ -0,0 +1,43 @@
+#ifndef GNARK_BIN_PUBLIC_WRITER_H_
+#define GNARK_BIN_PUBLIC_WRITER_H_
+
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/types/span.h"
+
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/errors.h"
+#include "zkx/base/auto_reset.h"
+#include "zkx/base/buffer/endian.h"
+#include "zkx/base/buffer/serde.h"
+#include "zkx/base/buffer/vector_buffer.h"
+
+namespace zkx::gnark {
+
+template <typename F>
+absl::Status WritePublicToBin(absl::Span<const F> public_values,
+                              const std::string& path) {
+  base::Uint8VectorBuffer write_buf;
+  size_t size = 0;
+  if (public_values.size() > 0) {
+    size += public_values.size() * base::EstimateSize(public_values[0]);
+  }
+  TF_RETURN_IF_ERROR(write_buf.Grow(size + 3 * sizeof(uint32_t)));
+  write_buf.set_endian(base::Endian::kBig);
+  base::AutoReset<bool> reset_scalar_field_is_in_montgomery(
+      &base::Serde<F>::s_is_in_montgomery, false);
+  TF_RETURN_IF_ERROR(write_buf.WriteMany(
+      static_cast<uint32_t>(public_values.size()), uint32_t{0},
+      static_cast<uint32_t>(public_values.size())));
+  for (size_t i = 0; i < public_values.size(); i++) {
+    TF_RETURN_IF_ERROR(write_buf.Write(public_values[i]));
+  }
+  std::string public_string(reinterpret_cast<const char*>(write_buf.buffer()),
+                            write_buf.buffer_offset());
+  return tsl::WriteStringToFile(tsl::Env::Default(), path, public_string);
+}
+
+}  // namespace zkx::gnark
+
+#endif  // GNARK_BIN_PUBLIC_WRITER_H_
