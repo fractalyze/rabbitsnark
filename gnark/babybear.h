08c9938a3d473f53561cfe24f9a4c17ee09d09a7
Ryan Kim <chokobole33@gmail.com>
2025-11-18 14:17:42 +0900

    chore: add license & missing license notices to source files
    Adds the Apache License, Version 2.0 license header notice to all source
    files within the rabbitsnark repository.

--- gnark/babybear.h
+++ gnark/babybear.h
@@ -1,3 +1,18 @@
+/* Copyright 2025 The RabbitSNARK Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 #include <stdint.h>
 uint32_t babybearextinv(uint32_t a, uint32_t b, uint32_t c, uint32_t d,
                         uint32_t i);
